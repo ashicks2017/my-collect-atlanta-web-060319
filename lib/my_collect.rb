6e873881a133337e2372325292b35164ae3a8692
aeisha simmons <ashicks2017@outlook.com>
2019-05-20 00:25:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
-i = 0
-result = []
-while i < array.length
-resuls <<(yield array[i])
-i +=1
+  i = 0
+  result = []
+  while i < array.length
+    result <<(yield array[i])
+    i +=1
+  end
+ result
 end
-
 
